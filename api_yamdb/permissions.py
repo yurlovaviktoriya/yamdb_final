4a416ea3840c9e82c3df1d926b424fdb88a5a6c9
Victoriya Yurlova <yurlovaviktoriya@yandex.ru>
2020-12-22 00:28:14 +0700

    pep 8 third

--- api_yamdb/permissions.py
+++ api_yamdb/permissions.py
@@ -11,27 +11,25 @@ class ReadOnlySafeMethods(permissions.BasePermission):
 
 class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return (request.user.is_authenticated 
-                and obj.author == request.user)
+        return (request.user.is_authenticated and
+                    obj.author == request.user)
 
 
 class IsModerator(permissions.BasePermission):
     def has_permission(self, request, view):
-        return (request.user.is_authenticated 
-                and request.user.is_moderator)
+        return (request.user.is_authenticated and
+                    request.user.is_moderator)
 
     def has_object_permission(self, request, view, obj):
-        return (request.user.is_authenticated 
-                and request.user.is_moderator)
+        return (request.user.is_authenticated
+                    and request.user.is_moderator)
 
 
 class IsAdminOrStaff(permissions.BasePermission):
     def has_permission(self, request, view):
-        return (request.user.is_authenticated 
-                and (request.user.is_admin or request.user.is_staff)
-                )
+        return (request.user.is_authenticated and
+                (request.user.is_admin or request.user.is_staff))
 
     def has_object_permission(self, request, view, obj):
-        return (request.user.is_authenticated 
-                and (request.user.is_admin or request.user.is_staff)
-                )
+        return (request.user.is_authenticated and
+                    (request.user.is_admin or request.user.is_staff))
